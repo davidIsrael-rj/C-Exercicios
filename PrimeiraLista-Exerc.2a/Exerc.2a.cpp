c3834999b50ba0ccce261e01f1cdf50b3ccf8c22
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-02 18:05:24 -0300

    Exerc2a

---
+++ PrimeiraLista-Exerc.2a/Exerc.2a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/*
+	Altere o programa do exercício 01 para que cada palavra da frase
+	Aprendendo a programar na Linguagem C++ apareça em linhas diferentes.
+*/
+int main()
+{
+	std::cout
+		<< "Aprendendo" << std::endl
+		<< "a" << std::endl
+		<< "programar" << std::endl
+		<< "na" << std::endl
+		<< "Linguagem C++" << std::endl;
+	system("PAUSE");
+	return 0;
+}
