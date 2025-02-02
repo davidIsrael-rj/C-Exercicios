dd7dedd0e513f4a065c0949d91d696bdfc676972
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-02 18:20:45 -0300

    Execução do Exercício da primeira lista 3

---
+++ PrimeiraLista-Exerc.3/Exerc3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/*
+	Utilizando o comando System(COLOR CorFundoCorTexto) e altere a cor de exibição da
+fonte no terminal de saída para uma cor de sua preferência.
+*/
+int main() {
+	system("color 26");
+	std::cout << "Aprendendo a programar na Linguagem C++" << std::endl;
+	system("PAUSE");
+	return 0;
+}
