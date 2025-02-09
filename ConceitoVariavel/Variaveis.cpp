5e1cb1181fde6165c0b56c4655b8aa003a1c9e0e
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-08 21:05:11 -0300

    Visualizando o tamanho da variável

--- ConceitoVariavel/Variaveis.cpp
+++ ConceitoVariavel/Variaveis.cpp
@@ -2,12 +2,18 @@
 #include <tchar.h>
 int main()
 {
+	//Função que configura o console windows para mostrar acentos
 	_tsetlocale(LC_ALL, _T("portuguese"));
+
+	
 	int NumVidas = 5;
 	int Pontuacao = 1350;
 	std::cout << "*********INICIO DO JOGO************" << std::endl;
 	std::cout << "Vidas Jogador: " << NumVidas << std::endl;
 	std::cout << "Pontuação: " << Pontuacao << std::endl;
+	std::cout << "Tamanho da Variável NumVidas: " << sizeof(NumVidas) <<" Bytes"<< std::endl;
+	std::cout << "Tamanho da Variável Pontuação: " << sizeof(Pontuacao) <<" Bytes" << std::endl;
+
 	std::cout << "Endereço que NumVidas Ocupa na Memória RAM: " << &NumVidas << std::endl;
 	std::cout << "Endereço que Pontuação Ocupa na Memória RAM: " << &Pontuacao << std::endl;
 	std::cout << "*********************" << std::endl;
