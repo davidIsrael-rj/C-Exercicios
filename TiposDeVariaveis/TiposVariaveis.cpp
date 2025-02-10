1683b96de79ef258407e3b42aa1d65499c3ed18a
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-10 20:31:41 -0300

    Tipos de Variáveis = double

--- TiposDeVariaveis/TiposVariaveis.cpp
+++ TiposDeVariaveis/TiposVariaveis.cpp
@@ -6,22 +6,26 @@ int main()
 
 	int Numero;
 	float Numero2;
-
+	double Numero3;
 
 	Numero = 40;
 	Numero2 = 55.56f;
-
+	Numero3 = 45345.904555;
 	std::cout << "Valor Número: " << Numero << std::endl;
 	std::cout << "Tamanho da Variável Número: " << sizeof(Numero) << " Bytes" << std::endl;
 	std::cout << "Endereço carregado na Memória: " << &Numero << std::endl;
 	
-	std::cout << "**************************" << std::endl;
+	std::cout << "**************************" <<std::endl;
 
 	std::cout << "Valor Número2: " << Numero2 << std::endl;
-	std::cout << "Tamanho da Variável Número2: " << sizeof(Numero2) << " Bytes" << std::endl;
+	std::cout << "Tamanho da Variável Número2: " << sizeof(Numero2)<<" Bytes" << std::endl;
 	std::cout << "Endereço carregado na Memória: " << &Numero2 << std::endl;
 
+	std::cout << "**************************" << std::endl;
 
+	std::cout << "Valor Número3: " << Numero3 << std::endl;
+	std::cout << "Tamanho da Variável Número3: " << sizeof(Numero3) << " Bytes" << std::endl;
+	std::cout << "Endereço carregado na Memória: " << &Numero3 << std::endl;
 
 	system("PAUSE");
 	return 0;
