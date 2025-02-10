cb58869040e67f5d5e55338af2902218a1d48492
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-10 20:14:24 -0300

    Criando duas variáveis e inserindo os valores e exibindo na tela

---
+++ Exerc_CriandoVariavel/Exec_CriandoVariaveis.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int NumComputadores;
+	int SerialComputadores;
+	NumComputadores = 100;
+	SerialComputadores = 11111;
+
+	std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
+	std::cout << "$\tO Valor NumComputadores : " << NumComputadores << "\t\t$"<<std::endl;
+	std::cout << "$\tO Valor SerialComputadores: " << SerialComputadores << "\t$" << std::endl;
+	std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
+	system("PAUSE");
+	return 0;
+}
