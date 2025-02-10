08546b9c96a4f7e390ea5212b70e1b90e020041e
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-10 20:29:17 -0300

    Tipos de Variáveis = float

--- TiposDeVariaveis/TiposVariaveis.cpp
+++ TiposDeVariaveis/TiposVariaveis.cpp
@@ -5,11 +5,24 @@ int main()
 	_tsetlocale(LC_ALL, _T("portuguese"));
 
 	int Numero;
+	float Numero2;
+
+
 	Numero = 40;
+	Numero2 = 55.56f;
 
 	std::cout << "Valor Número: " << Numero << std::endl;
-	std::cout << "Tamanho da Variável Número: " << sizeof(Numero)<<" Bytes" << std::endl;
+	std::cout << "Tamanho da Variável Número: " << sizeof(Numero) << " Bytes" << std::endl;
 	std::cout << "Endereço carregado na Memória: " << &Numero << std::endl;
+	
+	std::cout << "**************************" << std::endl;
+
+	std::cout << "Valor Número2: " << Numero2 << std::endl;
+	std::cout << "Tamanho da Variável Número2: " << sizeof(Numero2) << " Bytes" << std::endl;
+	std::cout << "Endereço carregado na Memória: " << &Numero2 << std::endl;
+
+
+
 	system("PAUSE");
 	return 0;
 
